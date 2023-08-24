d2d1fe1fc7962d8e47e80d0da59f018c4d1aaddc
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-24 20:01:31 +0300

    Create Flux.range.java

---
+++ Project Reactor/Flux_and_Mono/Flux.range.java
@@ -0,0 +1,14 @@
+`Flux.range(1, 5)` is a method provided by Project Reactor's `Flux` class that creates a `Flux` emitting a sequence of integers within a specified range. The two arguments to the method represent the starting value and the count of items to emit.
+
+In the given context, `Flux.range(1, 5)` generates a `Flux` that emits the integers from 1 to 5 in sequence. Here's a breakdown:
+
+- **Starting Value**: The first argument, `1`, specifies the starting value of the sequence.
+
+- **Count**: The second argument, `5`, specifies the count of items to emit.
+
+So, `Flux.range(1, 5)` emits the following integers: 1, 2, 3, 4, 5.
+
+You can use this `Flux` to create a stream of numbers for various purposes, such as testing, demonstration, or combining with other `Flux` operations like mapping, filtering, and more.
+
+
+
